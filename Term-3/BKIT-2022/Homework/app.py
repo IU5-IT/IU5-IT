0ffd174f843681c2a418d0c2820933e2cc851a8b
Dmitiy <dimapermyakov55@gmail.com>
2022-11-18 22:54:34 +0300

    

---
+++ Term-3/BKIT-2022/Homework/app.py
@@ -0,0 +1,20 @@
+# Copyright © 2022 mightyK1ngRichard <dimapermyakov55@gmail.com>
+
+from flask import Flask
+from src.functions import fibonacci
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Hello World'
+
+
+@app.route('/num/<number>')
+def num(number):
+    return str(list(fibonacci(int(number))))[1:-1]
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ Term-3/BKIT-2022/Homework/main.py
@@ -0,0 +1,5 @@
+# Copyright © 2022 mightyK1ngRichard <dimapermyakov55@gmail.com>
+
+
+if __name__ == "__main__":
+    ellipsis()
